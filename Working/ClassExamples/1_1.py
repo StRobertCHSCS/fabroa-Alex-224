50f42cd6b1bf6a61982bb3cf0bcb9afac84f88f8
Alex-224 <Alexander.yu22@ycdsbk12.ca>
2019-09-18 09:38:59 -0400

    small indie company

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,10 @@
+print("this is a print statement")
+print("what's wrong with this statement")
+print("how about this one")
+print(3.14, "is my favourite pi")
+print("here are the ages of my three children: ", 12, 7, 4)
+my_number = 50
+print(my_number)
+length = 20
+width = 30
+print("the dimensions of my yard are", length, "by", width)
