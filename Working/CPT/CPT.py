e4c2a40be8ad1235cbe4646d1d7c6272e771ae63
Alex-224 <Alexander.yu22@ycdsbk12.ca>
2020-01-23 09:46:26 -0500

    ugh

--- Working/CPT/CPT.py
+++ Working/CPT/CPT.py
@@ -1,5 +1,118 @@
 print("Welcome to eyedometrist")
-suspicion = input("do your eyes hurt")
+print("The answers can only be answered with yes or no \
+    in some occasions where numbers are needed, only type the number.")
 
-a = input("Do your eyes itch")
-b = input("How long are you on your computer daily: 1 = 1-3 hours  
+yes = True
+no =  False
+
+a =input("Are your eyes sore or blurry?")
+b =  input("Are your eyes watery or dry?")
+d = input("how long are you on a electronic device daily: \
+    1 = 1-3 hours, 2 = 2-4 hours, 3 = 5+ hours, 4 = don't use daily")
+e = input("Is your room or home a dry enviornment?")
+f =input("Do you get headaches?")
+h =  input("Do you wear eyewear?")
+i = input("Is your enviornment lit up properly?")
+j = input("Are you having a difficult time concentrating?")
+k = input("Are your muscles sore?")
+
+while d = 1 or 2: 
+    print("Let your eyes rest about every two hours that you use \
+        and electronic device")
+
+    if a + b = True:
+         print("Rest your eyes frequently and apply eyedrops occasionally")
+
+
+    elif a = True:
+        print("Apply eye drops or consider getting perscription glasses")
+
+    elif b = True:
+        print("Apply eye drops if your eyes are dry or watery")
+
+    elif e = True:
+        print("turn on air conditioning or open windows or use a humidifier")
+    
+    elif f = True:
+        print("limit time on electronic devices to reduce headaches and pain")
+    
+    elif h = True:
+        print("look into getting your perscription changed")
+
+    elif i = False:
+        print("light up your room properly to reduce straining your eyes")
+    
+    elif j = True:
+        print("just concentrate")
+
+    elif k = True:
+        print("Reduce the amount of stress toward your eyes to reduce \
+             sore muscles by stretching occasionally and minding your posture")
+
+
+while d = 3:
+    print("Reduce the time that you are on an electronic device \
+        or get blue light glasses to reduce damage to your eyes")
+        
+    if a + b = True:
+         print("Rest your eyes frequently and apply eyedrops occasionally")
+
+
+    elif a = True:
+        print("Apply eye drops or consider getting perscription glasses")
+
+    elif b = True:
+        print("Apply eye drops if your eyes are dry or watery")
+
+    elif e = True:
+        print("turn on air conditioning or open windows or use a humidifier")
+    
+    elif f = True:
+        print("limit time on electronic devices to reduce headaches and pain")
+    
+    elif h = True:
+        print("look into getting your perscription changed")
+
+    elif i = False:
+        print("light up your room properly to reduce straining your eyes")
+    
+    elif j = True:
+        print("just concentrate")
+
+    elif k = True:
+        print("Reduce the amount of stress toward your eyes to reduce \
+             sore muscles by stretching occasionally and minding your posture")
+
+while d = 4:
+
+    if a + b = True:
+         print("Rest your eyes frequently and apply eyedrops occasionally")
+
+
+    elif a = True:
+        print("Apply eye drops or consider getting perscription glasses")
+
+    elif b = True:
+        print("Apply eye drops if your eyes are dry or watery")
+
+    elif e = True:
+        print("turn on air conditioning or open windows or use a humidifier")
+    
+    elif f = True:
+        print("limit time on electronic devices to reduce headaches and pain")
+    
+    elif h = True:
+        print("look into getting your perscription changed")
+
+    elif i = False:
+        print("light up your room properly to reduce straining your eyes")
+    
+    elif j = True:
+        print("just concentrate")
+
+    elif k = True:
+        print("Reduce the amount of stress toward your eyes to reduce \
+             sore muscles by stretching occasionally and minding your posture")
+
+print("If your symptoms do not go away after the treatments above, \
+    go see a doctor")
